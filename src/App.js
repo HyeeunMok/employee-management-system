419c04717ffc7da799aa40f67944f4c55a50f9fb
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-02-27 14:04:25 -0500

    refactoring to Class component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
+/* eslint-disable consistent-return */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-deprecated */
+/* eslint-disable react/state-in-constructor */
 /* eslint-disable react/jsx-curly-brace-presence */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import NavBar from './components/UI/NavBar/NavBar';
 import SearchBar from './components/UI/SearchBar/SearchBar';
 import Spinner from './components/UI/Spinner/Spinner';
-import { fetchEmployeesAPI } from './api/FetchAPI';
 
-const App = () => {
-  const [state, setState] = useState({
+const url = 'http://localhost:8080/api/employees';
+class App extends React.Component {
+  state = {
     employees: [],
+    errorMessage: '',
     loading: true,
-  });
+  };
 
-  useEffect(() => {
-    fetchEmployeesAPI(setState);
-  }, []);
+  componentWillMount = () => {
+    fetch(url)
+      .then(response => response.json())
+      .then(employees => this.setState({ employees, loading: false }))
+      .catch(err =>
+        this.setState({
+          errorMessage: 'Fetch error. API is not available.',
+          loading: false,
+        }),
+      );
+  };
 
-  const { employees, loading } = state;
+  loadingHandler() {
+    if (this.state.loading) {
+      return <Spinner />;
+    }
+  }
 
-  return (
-    <div>
-      <NavBar title={'Plexxis'} user={'Hyeeun Mok'} />
-      <SearchBar />
-      <Spinner loading={loading} />
-      <h1>Employees List</h1>
-      {employees.map(employee => (
-        <div key={employee.id}>
-          {Object.keys(employee).map(key => (
-            <span key={key}>
-              {key}:{employee[key]}
-            </span>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
+  render() {
+    const { employees } = this.state;
 
+    console.log(this.state);
+
+    return (
+      <div>
+        <NavBar title={'Plexxis'} user={'Hyeeun Mok'} />
+        <SearchBar />
+        <h1>Employees List</h1>
+        {employees.map(employee => (
+          <div key={employee.id}>
+            {Object.keys(employee).map(key => (
+              <span key={key}>
+                {key}:{employee[key]}
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 export default App;
